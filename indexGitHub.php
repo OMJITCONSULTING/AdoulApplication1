206f1f8602020e9414bb31859b0d267167d5c648
OMJIT <omjitconsulting@gmail.com>
2024-01-02 00:58:28 -0500

    test 6

--- indexGitHub.php
+++ indexGitHub.php
@@ -1,9 +1,20 @@
 <?php
 /**
+ 
 *Plugin Name: IndexGitHub
-*Plugin URI: https://github.com/OMJITCONSULTING/AdoulApplication1
+*Plugin URI: https://omjit.com/adoul
 *Description: just test111111111
 *Author: Mounia
 *Version: 1.0.0
 *Author URI: https://omjit.com/adoul
 */
+
+add_action ('wp_footer','say_hello');
+
+function say_hello ()
+{
+    echo ('<p> Githuub tessssssssst 1 Fichier index <p>');
+    
+}
+
+?>
--- testGitHUB.php
+++ testGitHUB.php
@@ -1,9 +1,19 @@
 <?php
+
 /**
 *Plugin Name: TestGitHUB
-*Plugin URI: https://github.com/OMJITCONSULTING/AdoulApplication1
+*Plugin URI: https://omjit.com/adoul
 *Description: just test111111111
 *Author: Mounia
 *Version: 1.0.0
 *Author URI: https://omjit.com/adoul
 */
+
+add_action ('get_header','say_hello2');
+
+function say_hello2 ()
+{
+    echo ('<p> Githuub tessssssssst 2 Fichier TEST <p>');
+    
+}
+?>
