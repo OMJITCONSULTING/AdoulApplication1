0e93e60a4be8ae086993e11c4b0fbe29e6d99197
OMJIT <omjitconsulting@gmail.com>
2024-01-02 00:03:31 -0500

    test 2

--- indexGitHub.php
+++ indexGitHub.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package AdoulApplication
- * @version 1.0.0
+ * @version 1.1.2
  */
 /*
-Plugin Name: Test
+Plugin Name: indexGitHub
 Plugin URI: https://omjit.com/adoul
-Description: just test
+Description: just test2
 Author: Matt Mullenweg
-Version: 1.0.0
+Version: 1.1.2
 Author URI: https://omjit.com/adoul
 */
 
--- testGitHUB.php
+++ testGitHUB.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package AdoulApplication
- * @version 1.0.0
+ * @version 1.1.0
  */
 /*
-Plugin Name: Test
+Plugin Name: testGitHUB
 Plugin URI: https://omjit.com/adoul
-Description: just test
+Description: just test1
 Author: Matt Mullenweg
-Version: 1.0.0
+Version: 1.1.1
 Author URI: https://omjit.com/adoul
 */
 
